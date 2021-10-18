22399d25b0e7b5ea747ef58ec051760ca22abd85
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-10-18 18:30:20 -0300

    Realização do exercício

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props
+    const id  = this.props.location.pathname !== '/users' ? this.props.match.params.id : 0
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p>{ greetingsMessage}, my awesome Users component </p>
+        <p> Id do usuário: {id}</p>
       </div>
     );
   }
